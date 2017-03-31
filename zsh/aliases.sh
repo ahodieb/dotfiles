a83e98ce98b05f303701316f4d08cd874bf4ae05
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2017-03-31 11:20:11 +0100

    use gitup instead of gitx "http://gitup.co/"

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,6 +1,6 @@
 # Application Aliases
 alias sublime="open -n -a /Applications/Sublime\ Text.app"
-alias gitx="open -n -a /Applications/GitX.app"
+alias gitx="open -a /Applications/GitUp.app ."
 alias epoch="date +%s"
 
 alias zshconfig="sublime ~/.zshrc"
