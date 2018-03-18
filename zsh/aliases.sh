085ef54a363645873cdd0bd3eed94b58254e98c0
Abdallah Hodieb <hodieba@amazon.com>
2018-03-18 20:20:00 +0000

    Remove sublime from the aliases file

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,5 +1,4 @@
 # Application Aliases
-alias sublime="open -n -a /Applications/Sublime\ Text.app"
 alias gitx="open -a /Applications/GitX.app ."
 alias epoch="date +%s"
 alias zshconfig="sublime ~/.zshrc"
