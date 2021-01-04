7953b15fc93fd1c697e48de4d4f0ec885af9dceb
Abdallah Hodieb <hodieba@amazon.com>
2021-01-04 15:33:16 +0000

    ⚙️ Add NodeJs and Go runtimes configs

--- zsh/runtimes.sh
+++ zsh/runtimes.sh
@@ -7,6 +7,16 @@ USER_BASE_PATH=$(python3 -m site --user-base)
 export PATH=$PATH:$USER_BASE_PATH/bin
 ### -------------------------- ###
 
+### NVM for NodeJS ###
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+### -------------------------- ###
+
+### Include GOPATH to $PATH ###
+export PATH=$GOPATH:$PATH
+### -------------------------- ###
+
 ### Android SDK ###
 if [ -d "$HOME/Library/Android/sdk" ]; then
   export ANDROID_HOME=$HOME/Library/Android/sdk
