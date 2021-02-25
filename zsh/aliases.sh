5457406889febef095bdbb1cecb4e9d2f44fd1c6
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2021-02-25 16:03:02 +0000

    change alias from lg to gl (git log with pretty stuff)

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,5 +1,5 @@
 ## Git
-alias lg="git log --abbrev-commit --decorate --date=relative --format=format:'%C(red)%h%C(r) —— %C(bold blue)%an%C(r): %C(white)%s%C(r) %C(dim white) %C(bold green)(%ar)%C(r) %C(bold yellow)%d%C(r)' --all" # From: https://ahmadawais.com/show-emoji-git-log/
+alias gl="git log --abbrev-commit --decorate --date=relative --format=format:'%C(red)%h%C(r) —— %C(bold blue)%an%C(r): %C(white)%s%C(r) %C(dim white) %C(bold green)(%ar)%C(r) %C(bold yellow)%d%C(r)' --all" # From: https://ahmadawais.com/show-emoji-git-log/
 alias gc="git commit"
 alias gp="git pull --rebase --autostash"
 
