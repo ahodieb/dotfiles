060af02811fdc45d13543b4549d2e51ff6554fff
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2021-10-08 09:46:22 +0200

    🛠 add more tools to setup

--- setup-mac.sh
+++ setup-mac.sh
@@ -49,7 +49,7 @@ brew install pyenv
 pyenv install 3.9.0
 pyenv global 3.9.0
 eval "$(pyenv init -)"
-pip install requests pipenv matplotlib 
+pip install requests pipenv matplotlib tabulate cookiecutter
 ## NodeJs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash # https://github.com/nvm-sh/nvm#install--update-script
 nvm install --lts
