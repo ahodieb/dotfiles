285b23cc5dd3c69d9c348041d25f675a93c67cbd
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2022-01-12 01:49:13 +0000

    ♻️ cleanup runtimes

--- zsh/runtimes.sh
+++ zsh/runtimes.sh
@@ -1,21 +1,25 @@
+### Include GOPATH to $PATH ###
+export GOPATH=$HOME/.go
+export PATH=$GOPATH/bin:$PATH
+### -------------------------- ###
+
 ### Python bin folder ###
-if command -v pyenv 1>/dev/null 2>&1; then
+if [ -d "$HOME/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"   
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 fi
-
 USER_BASE_PATH=$(python3 -m site --user-base)
 export PATH=$PATH:$USER_BASE_PATH/bin
 ### -------------------------- ###
 
 ### NVM for NodeJS ###
-export NVM_DIR="$HOME/.nvm"
+if [ -d "$HOME/.pyenv" ]; then
+  export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-### -------------------------- ###
-
-### Include GOPATH to $PATH ###
-export GOPATH=$HOME/go
-export PATH=$GOPATH/bin:$PATH
+fi 
 ### -------------------------- ###
 
 ### Android SDK ###
@@ -26,15 +30,12 @@ if [ -d "$HOME/Library/Android/sdk" ]; then
 fi
 ### -------------------------- ###
 
-export DEV_TOOLS="$HOME/dev-tools"
-
 ### Flutter SDK ###
-if [ -d "$DEV_TOOLS/flutter" ]; then
-  export FLUTTER_HOME=$HOME/dev-tools/flutter
+if [ -d "$HOME/.flutter" ]; then
+  export FLUTTER_HOME="$HOME/.flutter"
   export PATH=$PATH:$FLUTTER_HOME/bin
 fi
 ### -------------------------- ###
-
 
 ###  GCP CLI & SDK  ###
 if [ -f "$DEV_TOOLS/google-cloud-sdk/path.zsh.inc" ]; then . "$DEV_TOOLS/google-cloud-sdk/path.zsh.inc"; fi
