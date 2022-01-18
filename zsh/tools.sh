fe85163d29088c3608a7005539c30061b1980f85
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2022-01-18 20:07:46 +0000

    🔥 remove zsh-autosuggestions and z from tools.sh because they are now in oh-my-zsh plugins

--- zsh/tools.sh
+++ zsh/tools.sh
@@ -1,11 +1,4 @@
 
-# zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions 
-[ -d "${HOME}/.zsh/zsh-autosuggestions" ] && source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-#Z - Jump around
-source ${HOMEBREW_PREFIX}/Cellar/z/1.9/etc/profile.d/z.sh
-
-### Papyrus ###
 export PAPYRUS_HOME="$HOME/.papyrus/bin"
 if [ -d "$PAPYRUS_HOME" ]; then
     export PATH=$PATH:$PAPYRUS_HOME
