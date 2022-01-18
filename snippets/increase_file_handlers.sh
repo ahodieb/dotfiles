9c452c8c8ca0c86ec4fb980e93f33eb1f5dabf0e
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2022-01-18 20:02:04 +0000

    🔥 move snippets into dedicated github gists

--- snippets/increase_file_handlers.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Increase the amount of max open files
-# Usually needed for nodjs!! :D
-echo "kern.maxfiles=20480" >> /etc/sysctl.conf
-echo "kern.maxfilesperproc=18000" >> /etc/sysctl.conf
