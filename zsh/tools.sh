d320c21889365fdd9af7629ff76c0ab03c40f8f0
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2022-01-18 20:12:15 +0000

    ♻️  change papyrus aliases from j to p

--- zsh/tools.sh
+++ zsh/tools.sh
@@ -1,11 +1,11 @@
 
-export PAPYRUS_HOME="$HOME/.papyrus/bin"
+# Papyrus -- github.com/ahodieb/papyrus
+local PAPYRUS_HOME="$HOME/.papyrus/bin"
 if [ -d "$PAPYRUS_HOME" ]; then
     export PATH=$PATH:$PAPYRUS_HOME
-    alias j="papyrus open"
-    alias vj="papyrus open --editor=vim"
+    alias p="papyrus open"
+    alias pvim="papyrus open --editor=vim"
 fi
-### -------------------------- ###
 
 # https://git-fork.com
 if [ -d "/Applications/Fork.app" ]; then
