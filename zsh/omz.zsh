cf94c103722469424a62307d867f5bf8374b92bf
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2023-10-15 23:38:56 +0100

    🔥 simplify omz config

--- zsh/omz.zsh
+++ zsh/omz.zsh
@@ -2,19 +2,12 @@ local OH_MY_ZSH_DIR=~/.oh-my-zsh
 if [ ! -d "${OH_MY_ZSH_DIR}" ]; then
   echo "⚠️  oh-my-zsh is not installed, install it from https://github.com/ohmyzsh/ohmyzsh#getting-started"
 else
-  DEFAULT_USER=$USER
-  HYPHEN_INSENSITIVE="true"
-  ZSH_DISABLE_COMPFIX=true
-  
-  # ZSH_THEME="agnoster"
+
   ZSH_THEME="powerlevel10k/powerlevel10k"
   plugins=(
     z
-    fzf
-    ripgrep
-    gnu-utils
-    zsh-autosuggestions
   )
 
+  source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
   source "${OH_MY_ZSH_DIR}/oh-my-zsh.sh"
 fi
