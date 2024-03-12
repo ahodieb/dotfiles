e84c152e612901a1c020d74fffad725d5a78f7ee
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2024-03-12 17:22:14 +0000

    ♻️  unifiy gitconfig

--- git/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Setting git configurations"
-
-# set global .gitignore
-echo "Setting git global gitignore"
-git config --global core.excludesfile $DOTFILES_DIR/git/global_git_ignore
-
