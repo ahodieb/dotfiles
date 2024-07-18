eab7be34515abceda57f09d5f11877067719ff75
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2024-07-18 14:40:12 +0100

    🎨 format telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -27,13 +27,12 @@ return
     end
   },
   {
-      "nvim-telescope/telescope-file-browser.nvim",
-      dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-      config = function()
-        require("telescope").load_extension "file_browser"
-        -- vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
-        vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
-      end
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").load_extension "file_browser"
+      -- vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
+      vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+    end
   },
 }
-
